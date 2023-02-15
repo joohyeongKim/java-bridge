bcb98cf898c714966af4da38c067b630d64a2a75
joohyeongKim <wnguddl96@knou.ac.kr>
2023-02-15 17:56:07 +0900

    feat(InputView) : '입력하라' 오퍼레이션 할당하는 책임의 '입력' 객체

--- src/main/java/bridge/ui/InputView.java
+++ src/main/java/bridge/ui/InputView.java
@@ -1,23 +1,41 @@
 package bridge.ui;
 
+import static bridge.ui.MessageType.READ_COMMAND;
+import static bridge.ui.MessageType.READ_MOVE;
+import static bridge.ui.MessageType.READ_SIZE;
+import static java.lang.Integer.parseInt;
+
+import bridge.domain.Validator;
 import bridge.interfaces.Input;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView implements Input {
+
+    private Validator inputValidator = new Validator();
+
     /**
      * 다리의 길이를 입력받는다.
      */
+    @Override
     public int readBridgeSize() {
-        return 0;
+        System.out.println(READ_SIZE.messaging());
+        String size = Console.readLine();
+        inputValidator.validateBridgeSize(size);
+        return parseInt(size);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
+    @Override
     public String readMoving() {
-        return null;
+        System.out.println(READ_MOVE.messaging());
+        String move = Console.readLine();
+        inputValidator.validateMove(move);
+        return move;
     }
 
     /**
@@ -25,7 +43,10 @@ public class InputView implements Input {
      */
     @Override
     public String readGameCommand() {
-        return null;
+        System.out.println(READ_COMMAND.messaging());
+        String command = Console.readLine();
+        inputValidator.validateCommand(command);
+        return command;
     }
 
 
