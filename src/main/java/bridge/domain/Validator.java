eb042c1dd324a9e0d6ac1efaaa05eb6f7e610981
joohyeongKim <wnguddl96@knou.ac.kr>
2023-02-15 19:21:57 +0900

    feat(Validator) : '검증기' 인터페이스의 구현체이자 '검증하라'라는 협력을 담당한 객체 추가

--- src/main/java/bridge/domain/Validator.java
+++ src/main/java/bridge/domain/Validator.java
@@ -1,10 +1,50 @@
 package bridge.domain;
 
+import static bridge.ui.MessageType.INVALID_COMMAND;
+import static bridge.ui.MessageType.INVALID_FORMAT;
+import static bridge.ui.MessageType.INVALID_MOVING;
+import static bridge.ui.MessageType.INVALID_RANGE;
+
 import bridge.interfaces.Validation;
+import java.util.List;
 
 public class Validator implements Validation {
-    @Override
-    public void validate() {
 
+    private static final int MINIMUM_SIZE = 3;
+    private static final int MAXIMUM_SIZE = 20;
+
+
+
+    @Override
+    public void validateBridgeSize(String bridgeSize) {
+        int size = toInteger(bridgeSize);
+        validateRange(size);
+
+    }
+
+    private int toInteger(String bridgeSize) {
+        try {
+            return Integer.valueOf(bridgeSize);
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_FORMAT.messaging());
+        }
+    }
+
+    public void validateRange(int size) {
+        if (size < MINIMUM_SIZE || size > MAXIMUM_SIZE) {
+            throw new IllegalArgumentException(INVALID_RANGE.messaging());
+        }
+    }
+
+    public void validateMove(String move){
+        if (!List.of("U", "D").contains(move)) {
+            throw new IllegalArgumentException(INVALID_MOVING.messaging());
+        }
+    }
+
+    public void validateCommand(String command) {
+        if (!List.of("R", "Q").contains(command)) {
+            throw new IllegalArgumentException(INVALID_COMMAND.messaging());
+        }
     }
 }
