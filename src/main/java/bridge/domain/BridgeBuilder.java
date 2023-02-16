49ed2a8064e99aace53abc1bc947d70b692205fc
joohyeongKim <wnguddl96@knou.ac.kr>
2023-02-16 15:13:06 +0900

    feat(BridgeBuilder) : 생성된 다리 정보를 포함한 enum 추가

---
+++ src/main/java/bridge/domain/BridgeBuilder.java
@@ -0,0 +1,14 @@
+package bridge.domain;
+
+public enum BridgeBuilder {
+
+    KEEP_MOVE("O"),
+    FALL("X");
+
+    private final String bridgeRow;
+
+    BridgeBuilder(String bridgeRow) {
+        this.bridgeRow = bridgeRow;
+    }
+
+}
