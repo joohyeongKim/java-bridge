96012523d21fa22ee14262a7a4d194fc86e80499
joohyeongKim <wnguddl96@knou.ac.kr>
2023-02-16 17:59:22 +0900

    refactor(MessageType) : 라운드 범위 메시지 추가

--- src/main/java/bridge/ui/MessageType.java
+++ src/main/java/bridge/ui/MessageType.java
@@ -3,8 +3,8 @@ package bridge.ui;
 public enum MessageType {
     INVALID_RANGE("[ERROR] 다리의 길이는 3 이상 20 이하입니다."),
     INVALID_FORMAT("[ERROR] 잘못된 형식입니다. 올바른 값을 입력해주세요."),
-    INVALID_MOVING("[ERROR] 이동할 칸을 선택해주세요 (위칸: U, 아래칸: D)"),
-    INVALID_COMMAND("[ERROR] 게임을 재시도 하거나 종료할지 선택해주세요. (재시도: R, 종료: Q)"),
+    INVALID_MOVING("[ERROR] 이동할 칸을 올바르게 입력해주세요 (위칸: U, 아래칸: D)"),
+    INVALID_COMMAND("[ERROR] 게임을 재시도 및 종료를 원하시면 올바르게 입력해주세요. (재시도: R, 종료: Q)"),
     READ_SIZE("\n다리의 길이를 입력해주세요."),
     READ_MOVE("\n이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     READ_COMMAND("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
