92ca05cedabd3da3ca5eb3086d91769e15667c19
joohyeongKim <wnguddl96@knou.ac.kr>
2023-02-20 17:45:46 +0900

    feat(BridgeGameRepository) : 다리의 상태를 관리하는 저장소 책임 추가

--- src/main/java/bridge/domain/BridgeGameRepository.java
+++ src/main/java/bridge/domain/BridgeGameRepository.java
@@ -1,2 +1,33 @@
-package bridge.domain;public class BridgeGameRepository {
+package bridge.domain;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class BridgeGameRepository {
+
+    private final Map<Round, BridgeBuilder> bridgeGameStatus = new HashMap<>();
+
+    public BridgeGameRepository() {
+    }
+
+    // 라운드와 다리 상태를 입력받아 추가
+    public void addStatus(Round round, BridgeBuilder bridgeBuilder) {
+        this.bridgeGameStatus.put(round, bridgeBuilder);
+    }
+
+    public void initialize() {
+        bridgeGameStatus.clear();
+    }
+
+    // 1라운드 부터의 다리를 반환
+    public List<BridgeBuilder> getBridge() {
+        return Round.naturalOrder().stream()
+                .map(bridgeGameStatus::get)
+                .filter(Objects::nonNull)
+                .collect(toList());
+    }
 }
