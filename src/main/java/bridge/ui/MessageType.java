f0a2c9693c73257e0b38ca7d8893dbd82449d22d
joohyeongKim <wnguddl96@knou.ac.kr>
2023-02-20 17:36:23 +0900

    refactor(MessageType) : 라운드 허용 범위에 대한 메시지 추가

--- src/main/java/bridge/ui/MessageType.java
+++ src/main/java/bridge/ui/MessageType.java
@@ -5,6 +5,7 @@ public enum MessageType {
     INVALID_FORMAT("[ERROR] 잘못된 형식입니다. 올바른 값을 입력해주세요."),
     INVALID_MOVING("[ERROR] 이동할 칸을 올바르게 입력해주세요 (위칸: U, 아래칸: D)"),
     INVALID_COMMAND("[ERROR] 게임을 재시도 및 종료를 원하시면 올바르게 입력해주세요. (재시도: R, 종료: Q)"),
+    INVALID_ROUND_RANGE("[ERROR] 허용된 라운드 범위를 벗어났습니다."),
     READ_SIZE("\n다리의 길이를 입력해주세요."),
     READ_MOVE("\n이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     READ_COMMAND("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
